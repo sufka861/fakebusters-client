2154b8880a51f6d5c65ee69dbb463281e0f9d443
ronina24 <roni24682@gmail.com>
2024-05-17 21:41:02 +0300

    firebase addition

--- src/views/dashboard/Analytics/index.tsx
+++ src/views/dashboard/Analytics/index.tsx
@@ -153,7 +153,7 @@ const Analytics = () => {
         }
     };
 
-    const handleFormChange = (name, value) => {
+    const handleFormChange = (name: any, value: any) => {
         setFormAnalysisData(prev => ({ ...prev, [name]: value }));
     };
 
